2cbf5d47acf7142e5f51ad469e081bca32dcd678
Kumail Ali Shaikh <kumailalidev@google.com>
2023-05-25 04:53:41 +0500

    18-Create a Generic Data Fetching Hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,47 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 // refer to API documentation for more  details.
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
-
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  // state variables
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // controller object
-    const controller = new AbortController();
-
-    setLoading(true);
-
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    // clean-up function
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres"); // hiding details about HTTP request (such as endpoints) behind a hook.
 
 export default useGenres;
