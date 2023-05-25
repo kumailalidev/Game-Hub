0ac7cc5cb2060cff3fa6737cb4d2db7c62181989
Kumail Ali Shaikh <kumailalidev@google.com>
2023-05-26 03:44:16 +0500

    27-Sorting the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,43 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    // - to reverse sort order
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  ); // currentSortOrder value is equal to sortOrder prop value
+
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         {/* optional chaining is used because the selectedPlatform value can be
       null. */}
-        Order by: Relevance
+        Order by: {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Date added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release Date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average Rating</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+          >
+            {" "}
+            {order.label}{" "}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/services/image-urls.ts
+++ src/services/image-urls.ts
@@ -1,6 +1,8 @@
 const getCroppedImageUrl = (url: string) => {
   const target = "media/";
 
+  if (!url) return ""; // if url is false return an empty string (null represent false)
+
   // index represent starting position of target (eg. media) parameter
   const index = url.indexOf(target) + target.length;
 
