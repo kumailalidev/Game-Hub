a674f90f521985cc8ebae2e0fc19e2bd1d66dc60
Kumail Ali Shaikh <kumailalidev@google.com>
2023-05-25 23:12:53 +0500

    21-Filtering Games by Genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,39 +1,50 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
+  // requestConfig is optional
   // state variables
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // controller object
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      // controller object
+      const controller = new AbortController();
 
-    setLoading(true);
+      setLoading(true);
 
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setData(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
+        });
 
-    // clean-up function
-    return () => controller.abort();
-  }, []);
+      // clean-up function
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  ); // when selectedGenre changes, another request is sended to server to get the games of selected genre.
 
   return { data, error, isLoading };
 };
